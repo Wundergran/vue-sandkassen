5c8af549f94e5c4fd086768ffeb1179d605508ff
frode <frode.standal94@gmail.com>
2018-02-10 17:19:13 +0100

    added firebaseui

--- Shoplist/src/App.vue
+++ Shoplist/src/App.vue
@@ -9,6 +9,18 @@
 export default {
   name: 'App'
 }
+
+// Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyBwPIvauRFSUOCdPqYfk-MXnItL_dRjBgY',
+    authDomain: 'shoplist-2f2a0.firebaseapp.com',
+    databaseURL: 'https://shoplist-2f2a0.firebaseio.com',
+    projectId: 'shoplist-2f2a0',
+    storageBucket: 'shoplist-2f2a0.appspot.com',
+    messagingSenderId: '275802183767'
+  };
+  firebase.initializeApp(config);
+
 </script>
 
 <style>
--- Shoplist/src/components/Login.vue
+++ Shoplist/src/components/Login.vue
@@ -15,4 +15,22 @@
         },
         methods: {}
     }
+
+    // FirebaseUI config.
+    var uiConfig = {
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>'
+    };
+
+    // Initialize the FirebaseUI Widget using Firebase.
+    var firebase = require('firebase');
+    var firebaseui = require('firebaseui');
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
 </script>
