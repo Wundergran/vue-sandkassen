f9169c322a2a0d5979b6723b441162665d82ed3a
frodstan <frode.standal94@gmail.com>
2018-02-10 14:47:47 +0100

    add login element

---
+++ Shoplist/src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+      <h3>Sign in</h3>
+      <input type="text" placeholder="Email"><br>
+      <input type="password" placeholder="Password"><br>
+      <Button>Login</Button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'login',
+        data: function() {
+            return{}
+        },
+        methods: {}
+    }
+</script>
